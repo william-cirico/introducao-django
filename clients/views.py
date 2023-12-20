2d5cf4b89c34a341a42575e66d6df3dc9c3aab33
William Círico <william.cirico@proway.com.br>
2023-12-20 19:36:32 -0300

    atual

--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Client
+from .forms import ClientForm
 
 # Create your views here.
 def index(request):
@@ -24,9 +25,29 @@ def detail(request, client_id):
     return render(request, 'detail.html', context)
     
 
-
 def create(request):
-    return HttpResponse("Criação")
+    # Método POST -> Enviando o formulário de cadastro
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        
+        context = {
+            "form": form
+        }
+        
+        return render(request, 'create.html', context)
+    
+    # Método GET -> Abrir a página para cadastrar
+    form = ClientForm()
+    
+    context = {
+        "form": form
+    }
+    
+    return render(request, 'create.html', context)
 
 
 def update(request, client_id):
@@ -34,4 +55,7 @@ def update(request, client_id):
 
 
 def delete(request, client_id):
-    return HttpResponse(f"Remover: {client_id}")
+    client = get_object_or_404(Client, pk=client_id)
+    client.delete()
+    
+    return HttpResponseRedirect(reverse("index"))
